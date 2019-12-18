37e04b246c436b9e173932a31bf0d2561fbadbad
maherlaaroussi <maher.laaroussi@gmail.com>
2019-12-18 14:42:17 +0100

    Initial project

--- src/main/scala/com/maherlaaroussi/iwanttogoout/Game.scala
+++ src/main/scala/com/maherlaaroussi/iwanttogoout/Game.scala
@@ -5,8 +5,7 @@ object Map {
   def apply(): Props = Props(new Map())
 }
 class Map extends Actor with ActorLogging {
-  var messages = List()
-  log.info("Generating map ...")
+  import Map._
   def receive: Receive = {
     case msg @ _ => log.info(s"Message : $msg")
   }
@@ -14,12 +13,10 @@ class Map extends Actor with ActorLogging {
 
 object Player {
   def apply(): Props = Props(new Player())
-  case class Message(msg: String)
 }
 class Player extends Actor with ActorLogging {
   import Player._
   def receive: Receive = {
-    case Message(msg) => log.info("Player: " + msg)
     case msg @ _ => log.info(s"Message : $msg")
   }
 }
@@ -29,6 +26,5 @@ object main extends App {
   val systeme = ActorSystem("simplesys")
   val map = systeme.actorOf(Map(), "map")
   val player = systeme.actorOf(Player(), "Maher")
-  //player ! Player.Message("Eh hop je rejoins la partie !")
 
 }
