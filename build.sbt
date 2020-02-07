016c1e25dc5d2da341098a6a492ed4b524b5e0ce
maherlaaroussi <maher.laaroussi@gmail.com>
2020-02-07 12:30:47 +0100

    Progress slowly and done some tests

--- build.sbt
+++ build.sbt
@@ -7,8 +7,9 @@ scalaVersion := "2.12.6"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.1.5",
-  "com.typesafe.akka" %% "akka-stream" % "2.5.16",
-  "com.typesafe.akka" %% "akka-actor" % "2.5.26"
+  "com.typesafe.akka" %% "akka-stream" % "2.5.19",
+  "com.typesafe.akka" %% "akka-actor" % "2.5.26",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
 )
 
 fork in run := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,28 @@
+import akka.stream.ActorMaterializer
 import com.maherlaaroussi.iwanttogoout.Game
+import akka.actor.{ActorLogging, ActorSystem}
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.Done
+import akka.http.scaladsl.marshalling.ToEntityMarshaller
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import io.swagger.server.api.DefaultApiMarshaller
+import io.swagger.server.model.Joueur
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
 
 object Main extends App {
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  object DefaultMarshaller extends DefaultApiMarshaller {
+    def toEntityMarshallerJoueur: ToEntityMarshaller[Joueur] = jsonFormat3(Joueur)
+  }
 
   System.exit(0)
 
