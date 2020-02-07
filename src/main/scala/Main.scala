6d31442ea08b25fd718945ac9c8df31b9bcffa85
maherlaaroussi <maher.laaroussi@gmail.com>
2020-02-07 13:10:15 +0100

    API working

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,16 +2,21 @@ import akka.stream.ActorMaterializer
 import com.maherlaaroussi.iwanttogoout.Game
 import akka.actor.{ActorLogging, ActorSystem}
 import akka.http.scaladsl.Http
+import io.swagger.server.model.Joueur
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
 import akka.stream.ActorMaterializer
 import akka.Done
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
-import io.swagger.server.api.DefaultApiMarshaller
+import io.swagger.server.api.{DefaultApi, DefaultApiMarshaller, DefaultApiService}
 import io.swagger.server.model.Joueur
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol._
+
+import scala.concurrent.Future
+import scala.io.StdIn
 
 
 object Main extends App {
@@ -24,6 +29,39 @@ object Main extends App {
     def toEntityMarshallerJoueur: ToEntityMarshaller[Joueur] = jsonFormat3(Joueur)
   }
 
-  System.exit(0)
+  object DefaultService extends DefaultApiService {
+    def joueurCreateNameGet(name: String)
+                 (implicit toEntityMarshallerJoueur: ToEntityMarshaller[Joueur]): Route = {
+      val reponse = Future {
+        Joueur(name, 0, "(0, 0)")
+      }
+      requestcontext => {
+        (reponse).flatMap {
+          (joueur: Joueur) =>
+            joueurCreateNameGet200(joueur)(toEntityMarshallerJoueur)(requestcontext)
+        }
+      }
+
+    }
+  }
+
+
+  val api = new DefaultApi(DefaultService, DefaultMarshaller)
+
+  val host = "localhost"
+  val port = 8080
+
+  val bindingFuture = Http().bindAndHandle(pathPrefix("api"){api.route}, host, port)
+  println(s"Server online at http://${host}:${port}/\nPress RETURN to stop...")
+
+  bindingFuture.failed.foreach { ex =>
+    println(s"${ex} Failed to bind to ${host}:${port}!")
+  }
+
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+
 
 }
